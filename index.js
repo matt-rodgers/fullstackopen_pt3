ba881fc12a736752fc005b552440400a41d7f2fd
Matt Rodgers <mattrodgers31@gmail.com>
2024-11-06 17:52:44 +0000

    fix bug with number vs string comparisons

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.get('/api/persons', (request, response) => {
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = request.params.id
+    const id = Number(request.params.id)
     const person = persons.find(p => p.id === id)
     if (person) {
         response.json(person)
