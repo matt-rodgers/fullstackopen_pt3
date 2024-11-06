08af4fda5ef501a98af7cea4dd3e2528cc0aee4b
Matt Rodgers <mattrodgers31@gmail.com>
2024-11-06 15:04:06 +0000

    part 3.4

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 
 app.use(express.json())
 
-const persons = [
+let persons = [
     { 
       "id": "1",
       "name": "Arto Hellas", 
@@ -38,6 +38,12 @@ app.get('/api/persons/:id', (request, response) => {
     } else {
         response.status(404).end()
     }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    persons = persons.filter(p => p.id !== id)
+    response.status(204).end()
 })
 
 app.get('/info', (request, response) => {
